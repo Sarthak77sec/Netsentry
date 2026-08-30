0f615f2e3f1bdbec60c841f90321d029cc06158d
Sarthak Sharma <sarthak6789sharma@gmail.com>
2026-08-30 23:21:19 +0530

    added the IPv6 packet capturing branching in the code

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,4 @@
-from scapy.all import sniff,IP,TCP,UDP,ICMP
+from scapy.all import sniff,IP,TCP,UDP,IPv6
 
 def start_sniffing(interface=None, count=0):
     sniff(iface=interface,prn=process_packet,count=count,store=False)
@@ -19,5 +19,24 @@ def process_packet(packet):
              dst_port=packet[UDP].dport
              print(f"UDP{src_ip}:{src_port}->{dest_ip}:{dst_port}")
 
+
         else:
             print(f"other IP proto:{src_ip}->{dest_ip}")
+
+    elif IPv6 in packet:
+            src_ip=packet[IPv6].src
+            dest_ip=packet[IPv6].dst
+    
+            if TCP in packet:
+                src_port=packet[TCP].sport
+                dst_port=packet[TCP].dport
+                flags=packet[TCP].flags
+                print(f"TCP6{src_ip}:{src_port}->{dest_ip}:{dst_port} flags={flags}")
+
+            elif UDP in packet:
+                 src_port=packet[TCP].sport
+                 dst_port=packet[UDP].dport
+                 print(f"UDP6 {src_ip}:{src_port}->{dest_ip}:{dst_port}")  
+            else:
+                 print(f"other IPV6 proto {src_ip}->{dest_ip}")  
+            
