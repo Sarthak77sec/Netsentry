8efc1b1e65b28016f062e6d0e59aedf24cd9f5b9
Sarthak Sharma <sarthak6789sharma@gmail.com>
2026-09-02 13:57:49 +0530

    update test.py

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-from scapy.all import sniff, IP
+from scapy.all import sniff,IP,TCP,UDP,ICMP
+def start_sniffing(interface=None, count=0):
+    sniff(iface=interface,prn=process_packet,count=count,store=False)
 
 def process_packet(packet):
     if IP in packet:
-        print(f"{packet[IP].src} -> {packet[IP].dst}")
+        src_ip= packet[IP].src
+        dest_ip=packet[IP].dst
 
-sniff(iface="eth0", prn=process_packet, count=10, store=False)
+        if TCP in packet:
+            src_port=packet[TCP].sport
+            dst_port=packet[TCP].dport
+            flags=packet[TCP].flags
+            print(f"TCP{src_ip}:{src_port}->{dest_ip}:{dst_port} flags={flags}")
+
+        elif UDP in packet:
+             src_port=packet[UDP].sport
+             dst_port=packet[UDP].dport
+             print(f"UDP{src_ip}:{src_port}->{dest_ip}:{dst_port}")
+
+        else:
+            print(f"other IP proto:{src_ip}:{dst_port}")
