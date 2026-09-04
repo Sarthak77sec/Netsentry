66abb0be4a0119d4fc60ddf77389676ab13dcdc4
Sarthak Sharma <sarthak6789sharma@gmail.com>
2026-09-04 23:07:06 +0530

    added connectivity from storage ipv6 protocol

--- sniffer.py
+++ sniffer.py
@@ -4,8 +4,6 @@ from db import insert_packet
 from datetime import datetime
 
 conn=sqlite3.connect("data/netsentry.db")
-def timestamp():
-     return
 
 def start_sniffing(interface=None, count=0):
     sniff(iface=interface,prn=process_packet,count=count,store=False)
@@ -28,12 +26,20 @@ def process_packet(packet):
              dst_port=packet[UDP].dport
              timestamp=str(datetime.now())
              print(f"UDP{src_ip}:{src_port}->{dest_ip}:{dst_port}")
-             insert_packet(conn,timestamp,src_ip,dest_ip,"UDPv4",src_port,dst_port,flags)
+             insert_packet(conn,
+                           timestamp,
+                           src_ip,
+                           dest_ip,"UDPv4",src_port,dst_port,flags)
 
 
         else:
             print(f"other IP proto:{src_ip}->{dest_ip}")
-            timestamp=str(datetime.now)
+            timestamp=str(datetime.now())
+            insert_packet(conn,timestamp,src_ip,dest_ip,
+                          "other proto",
+                          src_port=None,
+                          dst_port=None
+                          ,flags=None)
 
     elif IPv6 in packet:
             src_ip=packet[IPv6].src
@@ -43,12 +49,18 @@ def process_packet(packet):
                 src_port=packet[TCP].sport
                 dst_port=packet[TCP].dport
                 flags=packet[TCP].flags
+                timestamp=str(datetime.now())
                 print(f"TCP6{src_ip}:{src_port}->{dest_ip}:{dst_port} flags={flags}")
+                insert_packet(conn,timestamp,src_ip,dest_ip,"TCPv6",src_port,dst_port,flags)
 
             elif UDP in packet:
-                 src_port=packet[TCP].sport
+                 src_port=packet[UDP].sport
                  dst_port=packet[UDP].dport
-                 print(f"UDP6 {src_ip}:{src_port}->{dest_ip}:{dst_port}")  
+                 print(f"UDP6 {src_ip}:{src_port}->{dest_ip}:{dst_port}")
+                 timestamp=str(datetime.now())
+                 insert_packet(conn,timestamp,src_ip,dest_ip,"UDPv6",src_port,dst_port,flags)  
             else:
                  print(f"other IPV6 proto {src_ip}->{dest_ip}")  
+                 timestamp=str(datetime.now())
+                 insert_packet(conn,timestamp,src_ip,dest_ip,"other v6 proto",src_port=None,dst_port=None,flags=None)
             
