bd0e9cab144797773b82f51f7f148a48cdc9c0a9
Sarthak Sharma <sarthak6789sharma@gmail.com>
2026-09-15 20:09:00 +0530

    added icmp flood test functionality

--- rule_engine.py
+++ rule_engine.py
@@ -20,4 +20,23 @@ def check_port_scan(src_ip,window_seconds=10, port_threshold=15):
                 print(f"ALERT: Possible port scan from {src_ip} - {len(unique_ports)} ports in {window_seconds}s")
                 return True
     
-    return False 
+    return False
+
+icmp_activity=defaultdict(list)
+
+def record_icmp(src_ip):
+    now=time.time()
+    icmp_activity[src_ip].append(now)
+
+def check_icmp_flood(src_ip,window_seconds=5,packet_threshold=100):
+    now=time.time()
+    activity_list=icmp_activity[src_ip]
+
+    recent_entries=[entry for entry in activity_list if entry > now-window_seconds]
+
+    if len(recent_entries)>packet_threshold:
+          print(f"Alert:Possible ICMP flood {src_ip} - {len(recent_entries)} packets in {window_seconds}s")
+
+          return True
+    
+    return False
